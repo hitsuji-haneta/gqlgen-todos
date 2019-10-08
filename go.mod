cb9f324b7d8c46e552f8d7d75bdfecfd96818f97
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-10-08 20:14:46 +0900

    Generate gql server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hitsuji-haneta/gqlgen-todos
 
 go 1.12
+
+require github.com/99designs/gqlgen v0.10.1 // indirect
---
+++ resolver.go
@@ -0,0 +1,17 @@
+package gqlgen_todos
+
+import (
+	"context"
+) // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+type Resolver struct{}
+
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*Todo, error) {
+	panic("not implemented")
+}
